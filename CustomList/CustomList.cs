2628c261635a8fee287ac2bc7c1d5ec1157bfed6
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-01-23 16:16:06 -0600

    Added 5 Unit Tests to the add method

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        private int count = 0;
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        private int capacity = 1;
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+        public T[] customLists;
+        public CustomList()
+        {
+            this.customLists = new T[4];
+        }
+
+        public void CustomAdd(T value)
+        {
+            customLists[Count] = value;
+            count++;
+           
+        }
+    }
+}
---
+++ CustomListTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1298c14b-3f27-4561-be1f-8bda70a6d18e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
