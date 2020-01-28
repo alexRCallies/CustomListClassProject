067e3acabcd63047fbacbbe4a02f58017e47b991
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-01-28 10:13:39 -0600

    added two more combining lists test

--- CustomListTests/AddingListsToEachOtherTests.cs
+++ CustomListTests/AddingListsToEachOtherTests.cs
@@ -67,5 +67,65 @@ namespace CustomListTests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void AddingTwoListsTogetherToGetOneMEGALISTintOneListBiggerThanTheOther()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            CustomList<int> custom = new CustomList<int>();
+            CustomList<int> list = new CustomList<int>();
+            CustomList<int> expectedList = new CustomList<int>();
+            string expected;
+            string actual;
+            expectedList.CustomAdd(1);
+            expectedList.CustomAdd(3);
+            expectedList.CustomAdd(5);
+            expectedList.CustomAdd(2);
+            expectedList.CustomAdd(4);
+            expectedList.CustomAdd(6);
+            //Act
+            custom.CustomAdd(1);
+            custom.CustomAdd(3);
+            list.CustomAdd(5);
+            list.CustomAdd(2);
+            list.CustomAdd(4);
+            list.CustomAdd(6);
+            customList = custom + list;
+            expected = expectedList.ToString();
+            actual = customList.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AddingTwoListsTogetherToGetOneMEGALISTintDuplicateNumbers()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            CustomList<int> custom = new CustomList<int>();
+            CustomList<int> list = new CustomList<int>();
+            CustomList<int> expectedList = new CustomList<int>();
+            string expected;
+            string actual;
+            expectedList.CustomAdd(1);
+            expectedList.CustomAdd(3);
+            expectedList.CustomAdd(5);
+            expectedList.CustomAdd(4);
+            expectedList.CustomAdd(4);
+            expectedList.CustomAdd(6);
+            //Act
+            custom.CustomAdd(1);
+            custom.CustomAdd(3);
+            custom.CustomAdd(5);
+            list.CustomAdd(4);
+            list.CustomAdd(4);
+            list.CustomAdd(6);
+            customList = custom + list;
+            expected = expectedList.ToString();
+            actual = customList.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
